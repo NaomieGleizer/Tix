8f157c90901dcc409ea406e8c6c09ad66793f5d5
Zoe Milbauer <zoemilb@gmail.com>
2020-03-17 19:53:25 +0200

    adding items to order

--- mobile_application/App_cordova/www/scripts/nfc_scanning_page.js
+++ mobile_application/App_cordova/www/scripts/nfc_scanning_page.js
@@ -1,6 +1,8 @@
 ﻿document.addEventListener("DOMContentLoaded", function (event) {
     
-    var nfc_code = 1;
-    window.location.href = "restaurant_page.html?nfc=" + nfc_code;
 
+        var nfc_code = 1;
+    window.location.href = "restaurant_connection_page.html?nfc=" + nfc_code;
 });
+
+
