ac2b1d6ae63d9c95ac4f294caec7968f55b9ef90
Zoe Milbauer <zoemilb@gmail.com>
2020-04-02 19:31:26 +0300

    feature added: selecting size and choice to meal

--- mobile_application/App_cordova/www/scripts/restaurant_connection_page.js
+++ mobile_application/App_cordova/www/scripts/restaurant_connection_page.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // if page has been load already before, load menu
     if (sessionStorage.getItem("restaurant_menu")) {
         data = JSON.parse(sessionStorage.getItem("restaurant_menu"));
-        load_menu(data);
+        show_menu(data);
     }
     // else, sent a request to server to get menu
     else {
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             if (xhr.readyState === 4 && xhr.status === 200) {
                 var data = JSON.parse(xhr.responseText);
                 sessionStorage.setItem("restaurant_menu", JSON.stringify(data));
-                load_menu(data);
+                show_menu(data);
             }
         }
         //     xhr.setRequestHeader("Origin", 'naomie');
@@ -61,23 +61,74 @@ else {
 }
 
 // add item to order 
-function add_item_to_order(button_element) {
-    id = button_element.id;
-    item = document.getElementById("item_name" + id).innerText;
-    price = parseFloat(document.getElementById("item_price" + id).innerText);
-    // if item is in order, increase sum in 1
-    if (item in client_order) {
-        client_order[item][0] += price;
-        client_order[item][1] += 1;
-
+function add_item_to_order(button_element, meal) {
+    var id = (button_element.id).slice(-1);
+    var item = meal.item_name;
+    var price = meal.item_price;
+    var sizes = meal.item_sizes;
+    var choices = meal.item_choices;
+    var size = null;
+    var choice = null;
+    // if meal has sizes, check what user chose
+    if (sizes) {
+        var sizes_radio = document.getElementById("form" + String(id)).size;
+        for (var i = 0; i < sizes_radio.length; i++) {
+            if (sizes_radio[i].checked) {
+                size = sizes_radio[i].value;
+                size = size.split(" ")[0];
+                price = (price.split(",").reverse())[i];
+                (document.getElementById(item + "size" + String(i))).checked = false;
+            }
+        }
+        // if user did not choose size, alert
+        if (!size) {
+            alert("לא נבחר גודל מנה");
+            return;
+        }
     }
-    // add item to order, element 0 is price, 1 is number of orders of this meal
+    // if meal has sizes, check what user chose
+    if (choices) {
+        var choices_radio = document.getElementById("form" + String(id)).choice;
+        for (var i = 0; i < choices_radio.length; i++) {
+            if (choices_radio[i].checked) {
+                choice = choices_radio[i].value;
+                (document.getElementById(item + "choice" + String(i))).checked = false;
+            }
+        }
+        // if user did not choose choice, alert
+        if (!choice) {
+            alert("לא נבחרו אפשרויות");
+            return;
+        }
+    }
+    price = parseFloat(price);
+    // if item is in order, add it to list 
+    if (item in client_order) {
+        var to_push = true;
+        // check if meal is in order with same choice and size
+        for (i in client_order[item]) {
+            // if yes, increase price and number of meal 
+            if (client_order[item][i].includes(size) && client_order[item][i].includes(choice)) {
+                client_order[item][i][0] += price;
+                client_order[item][i][1] += 1;
+                to_push = false;
+                break;
+            }
+        }
+        // if not, add to list 
+        if (to_push) {
+            client_order[item].push([price, 1, price, size, choice]);
+        }
+    }
+    // else, add the meal to dictionary
     else {
-        client_order[item] = [price, 1, price];     
+        client_order[item] = [];
+        client_order[item].push([price, 1, price, size, choice]);
     }
     num_of_items++;
     // update number of items in my order button
     document.getElementById("my_order").innerText = "ההזמנה שלי: " + String(num_of_items);
+    document.getElementById("select_div" + id).style.display = "none";
 
     //var keyName = window.sessionStorage.key(0); //Get key name
     //window.sessionStorage.setItem("key", "value"); //Set item
@@ -126,7 +177,17 @@ function load_menu(data) {
             // botton div
             var item_add_btn = document.createElement("div");
             item_add_btn.className = "add";
-            item_add_btn.innerHTML = '<button class="add_btn" id= ' + String(id) + ' onclick="add_item_to_order(this)"  >+</button>';
+            var add_btn = document.createElement("BUTTON");
+            add_btn.setAttribute("id", String(id));
+            add_btn.setAttribute("class", "add_btn");
+            add_btn.onclick = (function () {
+                var button = add_btn;
+                return function () {
+                    select_size_and_choices(button);
+                }
+            })();
+            add_btn.innerHTML = "+";
+            item_add_btn.appendChild(add_btn);
             item_div.appendChild(item_add_btn);
             // name and price div  
             var name_price_div = document.createElement("div");
@@ -150,7 +211,187 @@ function load_menu(data) {
             item_description.className = "item_description";
             item_description.innerHTML = category.menu_items[j].item_description;
             item_content.appendChild(item_description);
+            // div for sizes and choices
+            var adding_item_div = document.createElement("div");
+            adding_item_div.setAttribute("id", "select_div" + String(id));
+            adding_item_div.style.display = "none";
+            adding_item_div.style.width = '100%';
+            add_size_and_choices_form(id, category.menu_items[j], adding_item_div);
+            item_content.appendChild(adding_item_div);
             id++;
         }
     }
+}
+
+// add form to sizes and choices of meal
+function add_size_and_choices_form(id, meal, adding_item_div) {
+    var paragraph = document.createElement("p");
+    var linebreak = document.createElement("br");
+    var sizes_choices_form = document.createElement("FORM");
+    sizes_choices_form.setAttribute("id", "form" + String(id));
+    var sizes_string = meal.item_sizes;
+    var choices_string = meal.item_choices;
+    // if there are sizes to meal, add selection
+    if (sizes_string) {
+        var sizes = sizes_string.split(",");
+        var prices = meal.item_price.split(",").reverse();
+        // go over sizes and add to form
+        for (j in sizes) {
+            var option = document.createElement("input");
+            option.setAttribute("type", "radio");
+            option.setAttribute("name", "size");
+            option.setAttribute("id", meal.item_name + "size" + String(j));
+            var description = sizes[j] + " ( ₪" + prices[j] + " )";
+            option.setAttribute("value", description);
+            var label = document.createElement("label");
+            label.setAttribute("for", "size");
+            var text = document.createTextNode(description);
+            var linebreak = document.createElement("br");
+            label.appendChild(option);
+            label.appendChild(text);
+            label.appendChild(linebreak);
+            sizes_choices_form.appendChild(label);
+        }
+    }
+    // if there are choices for meal, add selection
+    if (choices_string) {
+        var choices = choices_string.split(",");
+        // go over choices and add to form
+        for (j in choices) {
+            var option = document.createElement("input");
+            option.setAttribute("type", "radio");
+            option.setAttribute("name", "choice");
+            option.setAttribute("id", meal.item_name + "choice" + String(j));
+            var description = choices[j];
+            option.setAttribute("value", description);
+            var label = document.createElement("label");
+            label.setAttribute("for", "choice");
+            var text = document.createTextNode(description);
+            var linebreak = document.createElement("br");
+            label.appendChild(option);
+            label.appendChild(text);
+            label.appendChild(linebreak);
+            sizes_choices_form.appendChild(label);
+        }
+    }
+    sizes_choices_form.appendChild(paragraph);
+    // button for adding meal to order
+    adding_item_div.appendChild(sizes_choices_form)
+    var add_btn = document.createElement("BUTTON");
+    add_btn.setAttribute("id", "add_item" + String(id));
+    add_btn.setAttribute("class", "add_to_order");
+    add_btn.onclick = (function () {
+        var button = add_btn;
+        var m = meal;
+        return function () {
+            add_item_to_order(button, m);
+        }
+    })();
+    add_btn.innerHTML = "הוספת מנה";
+    adding_item_div.appendChild(add_btn);
+    // button for closing adding meal  
+    var close_btn = document.createElement("BUTTON");
+    close_btn.setAttribute("id", "close_btn" + String(id));
+    close_btn.setAttribute("class", "add_to_order");
+    close_btn.onclick = (function () {
+        var button = close_btn;
+        return function () {
+            close_adding_meal(adding_item_div);
+        }
+    })();
+    close_btn.innerHTML = "סגור";
+    adding_item_div.appendChild(close_btn);
+    adding_item_div.appendChild(linebreak);
+}
+
+// select size and choices for meal
+function select_size_and_choices(button_element) {
+    document.getElementById("select_div" + button_element.id).style.display = "table";
+    
+}
+
+// close adding meal div
+function close_adding_meal(adding_item_div) {
+    adding_item_div.style.display = "none";
+}
+
+// show restaurant menu 
+function show_menu(data) {
+    var welcome = document.getElementById("welcome");
+    restaurant_name = data.restaurant_name;
+    sessionStorage.setItem("restaurant_name", restaurant_name);
+    welcome.innerHTML = " ברוכים הבאים ל" + restaurant_name;
+    var table_menu = document.createElement("table");
+    table_menu.setAttribute("id", "menu_table");
+    var id = 0;
+    // go over categories in menu 
+    for (i in data.categories) {
+        // category name td
+        var category = data.categories[i];
+        var category_tr = document.createElement("tr");
+        var catrgory_th = document.createElement("td");
+        catrgory_th.setAttribute("class", "category_name");
+        catrgory_th.style.width = '100%';
+        catrgory_th.appendChild(document.createTextNode(String(category.name)));
+        category_tr.appendChild(catrgory_th);
+        table_menu.appendChild(category_tr);
+        // go over meals in category and add to table
+        for (j in category.menu_items) {
+            var meal_tr = document.createElement("tr");
+            // meal name and description
+            var td_meal = document.createElement("td");
+            td_meal.style.width = '70%';
+            td_meal.setAttribute("class", "item_name");
+            td_meal.setAttribute("id", "item_name" + String(id));
+            td_meal.appendChild(document.createTextNode(category.menu_items[j].item_name));
+            // if there is description to meal, add it 
+            if (category.menu_items[j].item_description) {
+                var linebreak = document.createElement("br");
+                td_meal.appendChild(linebreak);
+                var span = document.createElement('span');
+                span.style.fontSize = "12px";
+                span.appendChild(document.createTextNode(category.menu_items[j].item_description));
+                td_meal.appendChild(span);
+            }
+            meal_tr.appendChild(td_meal);
+            // meal price 
+            var td_price = document.createElement("td");
+            td_price.style.width = '15%';
+            td_price.setAttribute("class", "item_name");
+            td_price.setAttribute("id", "item_price" + String(id));
+            td_price.appendChild(document.createTextNode(category.menu_items[j].item_price + "₪"));
+            meal_tr.appendChild(td_price);
+            // adding meal to order button
+            var td_add_btn = document.createElement("td");
+            td_add_btn.style.width = '10%';
+            td_add_btn.setAttribute("class", "add");
+            var add_btn = document.createElement("BUTTON");
+            add_btn.setAttribute("id", String(id));
+            add_btn.setAttribute("class", "add_btn");
+            add_btn.onclick = (function () {
+                var button = add_btn;
+                return function () {
+                    select_size_and_choices(button);
+                }
+            })();
+            add_btn.innerHTML = "+";
+            td_add_btn.appendChild(add_btn);
+            meal_tr.appendChild(td_add_btn);
+            // adding meal - select size and choices 
+            var adding_meal_tr = document.createElement("tr");
+            var td_adding_meal = document.createElement("td");
+            var adding_item_div = document.createElement("div");
+            adding_item_div.setAttribute("id", "select_div" + String(id));
+            adding_item_div.style.display = "none";
+            adding_item_div.style.width = '100%';
+            // add form to select size and choices
+            add_size_and_choices_form(id, category.menu_items[j], adding_item_div);
+            td_adding_meal.appendChild(adding_item_div);
+            adding_meal_tr.appendChild(td_adding_meal);
+            table_menu.appendChild(meal_tr);
+            table_menu.appendChild(adding_meal_tr);
+            id++;
+        }
+    }
+    document.getElementById("menu").appendChild(table_menu);
 }
--- mobile_application/App_cordova/www/scripts/viewing_order.js
+++ mobile_application/App_cordova/www/scripts/viewing_order.js
@@ -6,64 +6,71 @@
     var id = 0;
     // create a table with order items 
     for (var meal in meals) {
-        var tr = document.createElement("TR");
-        tr.setAttribute("id", "item" + String(id));
-        document.getElementById("order_items").appendChild(tr);
-        // meal name
-        var td_meal = document.createElement("TD");
-        td_meal.setAttribute("id", "meal" + String(id));
-        td_meal.style.width = '70%';
-        var text = document.createTextNode(meal + ' ' + String(meals[meal][1]) + 'x');
-        td_meal.appendChild(text);
-        document.getElementById("item" + String(id)).appendChild(td_meal);
-        // meal price
-        var td_price = document.createElement("TD");
-        td_price.setAttribute("id", "price" + String(id));
-        td_price.style.width = '10%';
-        var text2 = document.createTextNode(String(meals[meal][0]));
-        td_price.appendChild(text2);
-        document.getElementById("item" + String(id)).appendChild(td_price);
-        // add meal button
-        var td_btn_add = document.createElement("TD");
-        td_btn_add.style.width = '10%';
-        td_btn_add.setAttribute("id", "td+" + String(id));
-        var btn2 = document.createElement("BUTTON");
-        btn2.setAttribute("id", String(id) + "+");
-        btn2.setAttribute("class", "remove_btn");
-        btn2.innerHTML = "+";
-        btn2.onclick = (function () {
-            var m = meal;
-            var b = btn2;
-            var me = meals;
-            return function () {
-                add_item(b, me, m);
+        for (i in meals[meal]) {
+            var tr = document.createElement("TR");
+            tr.setAttribute("id", "item" + String(id));
+            document.getElementById("order_items").appendChild(tr);
+            // meal name
+            var td_meal = document.createElement("TD");
+            td_meal.setAttribute("id", "meal" + String(id));
+            td_meal.style.width = '80%';
+            var comments = "";
+            // if there is size to meal, add to comments
+            if (meals[meal][i][3]) {
+                comments += String(meals[meal][i][3]);
             }
-        })();
-        td_btn_add.appendChild(btn2);
-        document.getElementById("item" + String(id)).appendChild(td_btn_add);
-        // remove meal button
-        var td_btn_remove = document.createElement("TD");
-        td_btn_remove.style.width = '10%';
-        td_btn_remove.setAttribute("id", "td-" + String(id));
-        var btn = document.createElement("BUTTON");
-        btn.setAttribute("id", String(id) + "-");
-        btn.setAttribute("class", "remove_btn");
-        btn.innerHTML = "-";
-        btn.onclick = (function () {
-            var m = meal;
-            var b = btn;
-            var me = meals;
-            return function () {
-                remove_item_from_order(b, me, m);
+            // if there is choice to meal, add to comments
+            if (meals[meal][i][4]) {
+                if (comments != "") {
+                    comments += ", "
+                }
+                comments += "עם " + String(meals[meal][i][4]);
             }
-        })();
-        td_btn_remove.appendChild(btn);
-        document.getElementById("item" + String(id)).appendChild(td_btn_remove);
-        total_price += meals[meal][0];
-        id += 1;
+            var text = document.createTextNode(meal + ' ' + String(meals[meal][i][1]) + 'x');
+            td_meal.appendChild(text);
+            // if there are comments, add to table
+            if (comments != "") {
+                var linebreak = document.createElement("br");
+                td_meal.appendChild(linebreak);
+                var comments_text = document.createTextNode(comments);
+                var span = document.createElement('span');
+                span.style.fontSize = "12px";
+                span.appendChild(comments_text);
+                td_meal.appendChild(span);
+            }
+            document.getElementById("item" + String(id)).appendChild(td_meal);
+            // meal price
+            var td_price = document.createElement("TD");
+            td_price.setAttribute("id", "price" + String(id));
+            td_price.style.width = '10%';
+            var text2 = document.createTextNode(String(meals[meal][i][0]) + "₪");
+            td_price.appendChild(text2);
+            document.getElementById("item" + String(id)).appendChild(td_price);
+            // remove meal button
+            var td_btn_remove = document.createElement("TD");
+            td_btn_remove.style.width = '10%';
+            td_btn_remove.setAttribute("id", "td-" + String(id));
+            var btn = document.createElement("BUTTON");
+            btn.setAttribute("id", String(id) + "-");
+            btn.setAttribute("class", "remove_btn");
+            btn.innerHTML = "-";
+            btn.onclick = (function () {
+                var meal_name = meal;
+                var button = btn;
+                var me = meals;
+                var meal_element = meals[meal][i];
+                return function () {
+                    remove_item_from_order(button, me, meal_element, meal_name, comments);
+                }
+            })();
+            td_btn_remove.appendChild(btn);
+            document.getElementById("item" + String(id)).appendChild(td_btn_remove);
+            total_price += parseFloat(meals[meal][i][0]);
+            id += 1;
+        }
     }
     // update total price
-    document.getElementById("total_price").innerHTML = "סכום כולל: " + String(total_price);
+    document.getElementById("total_price").innerHTML = "סכום כולל: " + String(total_price) + "₪";
     sessionStorage.setItem("total_price", total_price);
 
     // add onclick events to buttons
@@ -75,7 +82,6 @@
 
 });
 
-
 // back to menu
 function back_to_menu() {
     window.location.href = "restaurant_connection_page.html";
@@ -86,33 +92,6 @@ function paying() {
     window.location.href = "purchase_summary.html";
 }
 
-// remove item from order
-function remove_item_from_order(button_element, meals, meal_name) {
-    var id = String(button_element.id).slice(0, -1);
-    var meal = meals[meal_name];
-    var meal_price = meal[2];
-    // if number of meal is bigger than 1, update
-    if (meal[1] > 1) {
-        document.getElementById("meal" + id).innerText = meal_name + " " + String(meal[1] - 1) + "x";
-        meals[meal_name][0] -= meal_price;
-        meals[meal_name][1] -= 1;
-        document.getElementById("price" + id).innerText = String(meal[0]);
-    }
-    // remove meal from order
-    else {
-        var elem = document.getElementById("item" + id);
-        elem.parentNode.removeChild(elem);
-        delete meals[meal_name];
-    }
-    // update total price and client order in session storage
-    total_price = sessionStorage.getItem("total_price") - meal_price;
-    sessionStorage.setItem("total_price", total_price);
-    document.getElementById("total_price").innerHTML = "סכום כולל: " + String(total_price);
-    sessionStorage.setItem("client_order_meals", JSON.stringify(meals));
-    var num_of_items = sessionStorage.getItem("num_of_items") - 1;
-    sessionStorage.setItem("num_of_items", num_of_items);
-}
-
 // add item to order
 function add_item(button_element, meals, meal_name) {
     var id = String(button_element.id).slice(0, -1);
@@ -121,13 +100,59 @@ function add_item(button_element, meals, meal_name) {
     document.getElementById("meal" + id).innerText = meal_name + " " + String(meal[1] + 1) + "x";
     meals[meal_name][0] += meal_price;
     meals[meal_name][1] += 1;
-    document.getElementById("price" + id).innerText = String(meal[0]);
- 
+    document.getElementById("price" + id).innerText = String(meal[0]) + "₪";
     // update total price and client order in session storage
     total_price = parseFloat(sessionStorage.getItem("total_price")) + parseFloat(meal_price);
     sessionStorage.setItem("total_price", total_price);
-    document.getElementById("total_price").innerHTML = "סכום כולל: " + String(total_price);
+    document.getElementById("total_price").innerHTML = "סכום כולל: " + String(total_price) + "₪";
     sessionStorage.setItem("client_order_meals", JSON.stringify(meals));
     var num_of_items = parseInt(sessionStorage.getItem("num_of_items")) + 1;
+    sessionStorage.setItem("num_of_items", num_of_items);
+}
+
+
+// remove item from order
+function remove_item_from_order(button_element, meals, meal, meal_name, comments) {
+    var id = String(button_element.id).slice(0, -1);
+    //var meal = meals[meal_name][index];
+    var meal_price = meal[2];
+    // if number of meal is bigger than 1, update
+    if (meal[1] > 1) {
+        document.getElementById("meal" + id).innerText = meal_name + " " + String(meal[1] - 1) + "x";
+        //document.getElementById("meal" + id).innerText = String(meal[1] - 1) + "x";
+        meal[0] -= meal_price;
+        meal[1] -= 1;
+        document.getElementById("price" + id).innerText = String(meal[0]) + "₪";
+        if (comments != "") {
+            var linebreak = document.createElement("br");
+            document.getElementById("meal" + id).appendChild(linebreak);
+            var comments_text = document.createTextNode(comments);
+            var span = document.createElement('span');
+            span.style.fontSize = "12px";
+            span.appendChild(comments_text);
+            document.getElementById("meal" + id).appendChild(span);
+        }
+    }
+    // remove meal from order
+    else {
+        var elem = document.getElementById("item" + id);
+        elem.parentNode.removeChild(elem);
+        if (meals[meal_name].length > 1) {
+            //delete meals[meal_name][index];
+            var index = meals[meal_name].indexOf(meal);
+            if (index !== -1) meals[meal_name].splice(index, 1);
+        }
+        else {
+            //var index = meals.indexOf(meal_name);
+            //if (index !== -1) meals.splice(index, 1);
+            delete meals[meal_name];
+        }
+    }
+    // update total price and client order in session storage
+    total_price = sessionStorage.getItem("total_price") - meal_price;
+    sessionStorage.setItem("total_price", total_price);
+    document.getElementById("total_price").innerHTML = "סכום כולל: " + String(total_price) + "₪";
+    sessionStorage.setItem("client_order_meals", JSON.stringify(meals));
+    var num_of_items = sessionStorage.getItem("num_of_items") - 1;
     sessionStorage.setItem("num_of_items", num_of_items);
 }
